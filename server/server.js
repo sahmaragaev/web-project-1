9804a8179fc1a507873d415032a639c082b1e311
Shahmar Aghayev <sahmaraqayev32@gmail.com>
2024-11-28 08:22:39 +0400

    Fix file naming for server

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get("/api/auth-status", async (req, res) => {
             return res.status(401).json({ authenticated: false, message: "Not logged in." });
         }
 
-        const client = await linkedInClientManager.getClient({ email, password });
+        const client = await ClientManager.getClient({ email, password });
         await client.ensureAuthenticated();
 
         res.json({ authenticated: true });
@@ -34,8 +34,7 @@ app.post("/api/login", async (req, res) => {
         email = reqEmail;
         password = reqPassword;
 
-        const client = await linkedInClientManager.getClient({ email, password });
-
+        const client = await ClientManager.getClient({ email, password });
         res.json({ message: "Login successful", cookies: client.cookies });
     } catch (error) {
         console.error("Error during login:", error.message);
@@ -52,7 +51,7 @@ app.get("/api/profile/:publicId", async (req, res) => {
             throw new Error("User is not logged in. Please log in first.");
         }
 
-        const client = await linkedInClientManager.getClient({ email, password });
+        const client = await ClientManager.getClient({ email, password });
         const profile = await client.getProfile(publicId);
         res.json(profile);
     } catch (error) {
@@ -61,7 +60,7 @@ app.get("/api/profile/:publicId", async (req, res) => {
     }
 });
 
-// Start Server
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
